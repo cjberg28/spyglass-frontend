8794fdc129a121d8288978651f087111c4a496a2
cjberg28 <cjberg28@yahoo.com>
2022-08-31 13:40:00 -0500

    Removed test username/password, replaced with environment variable

--- spyglass-ui/src/app/app.component.ts
+++ spyglass-ui/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UserService } from 'src/services/user.service';
 import { HttpClient, HttpClientModule, HttpResponse } from '@angular/common/http';
 import { GoalService } from 'src/services/goal.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -11,16 +12,14 @@ import { GoalService } from 'src/services/goal.service';
 export class AppComponent {
   title = 'spyglass-ui';
 
-  // userService: UserService;
-
   constructor(private userService: UserService, private goalService: GoalService) {}
 
   getAllUsers(): void {
-    this.userService.findByEmail('cberg@skillstorm.com', 'jchan@aol.com', 'jchan').subscribe({
+    this.userService.findByEmail('cberg@skillstorm.com', environment.TEST_USERNAME, environment.TEST_PASSWORD).subscribe({
       next: (data) => { console.log(data); },//data is an HttpResponse
       error: (error) => { console.log(error.error) }//error is an HttpErrorResponse
     });
-    this.goalService.findByUser('cberg@skillstorm.com', 'jchan@aol.com', 'jchan').subscribe({
+    this.goalService.findByUser('cberg@skillstorm.com', environment.TEST_USERNAME, environment.TEST_PASSWORD).subscribe({
       next: (data) => { console.log(data); },//data is an HttpResponse
       error: (error) => { console.log(error.error) }//error is an HttpErrorResponse
     });
