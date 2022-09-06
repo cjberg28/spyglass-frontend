be9915da6ba030549a4ee92dd6fb8d47bb90bd9e
cjberg28 <cjberg28@yahoo.com>
2022-09-06 18:10:37 -0500

    Homepage Component testing relatively complete, need to figure out how to wait for a subscribe to finish

--- spyglass-ui/src/app/homepage/homepage.component.spec.ts
+++ spyglass-ui/src/app/homepage/homepage.component.spec.ts
@@ -1,14 +1,36 @@
-import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { ProgressBarModule } from 'primeng/progressbar';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from 'src/models/user.model';
+import { GoalService } from 'src/services/goal.service';
+import {routes} from '../app-routing.module';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import {Location} from '@angular/common';
+import {Goal} from '../../models/goal.model'
 
 import { HomepageComponent } from './homepage.component';
+import { GoalSharingService } from 'src/services/goal-sharing.service';
 
 describe('HomepageComponent', () => {
   let component: HomepageComponent;
   let fixture: ComponentFixture<HomepageComponent>;
+  let goalService: GoalService;
+  // let mockGoalService = {
+  //   findByUser(email: string, username: string, password: string) {
+  //     return {
+  //       subscribe: () => console.log('MOCK SERVICE CALLED')
+  //     }
+  //   }
+  // }
+  let router: Router;
+  let location: Location;
+  // let goalSharingService: GoalSharingService;
+  
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,18 +38,177 @@ describe('HomepageComponent', () => {
       imports: [
         HttpClientModule,
         ProgressBarModule,
-        ButtonModule
+        ButtonModule,
+        RouterTestingModule.withRoutes(routes)
       ],
-      providers: [MessageService]
+      providers: [MessageService, Location]//, {provide: GoalSharingService, useValue: goalSharingService}]//, {provide: GoalService, useValue: mockGoalService}]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(HomepageComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);//Used for navigation.
+    location = TestBed.inject(Location);//Location is used to check the URL path.
+    router.initialNavigation();
     fixture.detectChanges();
+    goalService = TestBed.inject(GoalService);
+    // goalSharingService = TestBed.inject(GoalSharingService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should calculate progress for some mock data, where currentSum < targetSum', () => {
+    let testUser: User = new User('cberg@skillstorm.com','Cameron','Berg',new Date('2000-01-28'));
+    component.goals = [
+      {
+        id: 1,
+        name: 'Name 1',
+        description: 'Desc 1',
+        imageSrc: 'example.com/1',
+        targetDate: new Date('2022-10-24'),
+        targetAmount: 200,
+        currentAmount: 100,
+        userId: 'cberg@skillstorm.com',
+        user: testUser
+      },
+      {
+        id: 2,
+        name: 'Name 2',
+        description: 'Desc 2',
+        imageSrc: 'example.com/2',
+        targetDate: new Date('2022-10-24'),
+        targetAmount: 200,
+        currentAmount: 300,//This should only add 200, since that is the max amount (as dictated by target)
+        userId: 'cberg@skillstorm.com',
+        user: testUser
+      }
+    ];
+    component.calculateProgress();
+    expect(component.totalProgress).toEqual(75);
+  });
+
+  it('should calculate progress for some mock data, where currentSum >= targetSum', () => {
+    let testUser: User = new User('cberg@skillstorm.com','Cameron','Berg',new Date('2000-01-28'));
+    component.goals = [
+      {
+        id: 1,
+        name: 'Name 1',
+        description: 'Desc 1',
+        imageSrc: 'example.com/1',
+        targetDate: new Date('2022-10-24'),
+        targetAmount: 200,
+        currentAmount: 500,
+        userId: 'cberg@skillstorm.com',
+        user: testUser
+      },
+      {
+        id: 2,
+        name: 'Name 2',
+        description: 'Desc 2',
+        imageSrc: 'example.com/2',
+        targetDate: new Date('2022-10-24'),
+        targetAmount: 200,
+        currentAmount: 300,//This should only add 200, since that is the max amount (as dictated by target)
+        userId: 'cberg@skillstorm.com',
+        user: testUser
+      }
+    ];
+    component.calculateProgress();
+    expect(component.totalProgress).toEqual(100);
+  });
+
+  //Function calls that need to be tested are contained within a subscribe() to an observable.
+  //Figuring out how to wait for a subscribe to finish before continuing is complicated.
+  //I am yet to figure it out, hence these tests are commented out.
+  // it ('should get all goals when getGoals() is called', () => {
+  //   spyOn(goalService, 'findByUser').and.callThrough();
+  //   spyOn(component, 'calculateProgress').and.callThrough();
+  //   component.getGoals('jchan@aol.com',environment.TEST_USERNAME,environment.TEST_PASSWORD);
+  //   expect(goalService.findByUser).toHaveBeenCalled();
+  //   expect(component.calculateProgress).toHaveBeenCalled();
+  //   // expect(component.goals.length).toBeGreaterThan(0);
+  // });
+
+  // it ('should error when getGoals() is called with an incorrect email', () => {
+  //   spyOn(goalService, 'findByUser').and.callThrough();//callFake(fakeFindByUser);
+  //   spyOn(component, 'logout');
+  //   component.getGoals('jchan@aol.au',environment.TEST_USERNAME,environment.TEST_PASSWORD);
+  //   expect(goalService.findByUser).toHaveBeenCalled();
+  //   expect(component.logout).toHaveBeenCalled();
+  // });
+
+  it ('should navigate to the login page when logout() is called', fakeAsync(() => {
+    spyOn(component, 'clearCredentials');
+    component.logout();
+    expect(component.clearCredentials).toHaveBeenCalled();
+    tick();//Cause time to pass so the redirect happens
+    expect(location.path()).toBe('/login');
+  }));
+
+  it ('should clear credentials when clearCredentials() is called', () => {
+    component.clearCredentials();
+    component.getCredentials();//CredsService is private, so this is the way to access the values
+    expect(component.currentUser.firstName).toEqual('');
+    expect(component.currentUser.email).toBe('');
+    expect(component.currentUser.lastName).toBe('');
+    //Date should also be checked, but clearing creds sets the date to new Date(), so equality would generally never be true
+    expect(component.username).toBe('');
+    expect(component.password).toBe('');
+  });
+
+  it('should view a goal when a goal is clicked', fakeAsync(() => {
+    let goalSharingService = TestBed.inject(GoalSharingService);
+    spyOn(goalSharingService,'setGoal');
+    component.viewGoal(new Goal(1,'','','',new Date(),100,1,'',new User('','','',new Date())));
+    expect(goalSharingService.setGoal).toHaveBeenCalled();
+    tick();
+    expect(location.path()).toBe('/view-goal');
+  }));
+
+  it('should navigate to the create page when navigateToCreatePage() is called', fakeAsync(() => {
+    component.navigateToCreatePage();
+    tick();
+    expect(location.path()).toBe('/new-goal');
+  }));
 });
+
+//This is just testing with the commented out functions. Please ignore.
+
+// function fakeFindByUser(email: string, username: string, password: string): Observable<HttpResponse<any>> {
+//   let testUser: User = new User('cberg@skillstorm.com','Cameron','Berg',new Date('2000-01-28'));
+//   let response: HttpResponse<any>;
+//   if (username === 'jchan@aol.com') {
+//     let response = new HttpResponse<any>({
+//       body: [
+//         {
+//           id: 1,
+//           name: 'Name 1',
+//           description: 'Desc 1',
+//           imageSrc: 'example.com/1',
+//           targetDate: new Date('2022-10-24'),
+//           targetAmount: 200,
+//           currentAmount: 100,
+//           userId: 'cberg@skillstorm.com',
+//           user: testUser
+//         },
+//         {
+//           id: 2,
+//           name: 'Name 2',
+//           description: 'Desc 2',
+//           imageSrc: 'example.com/2',
+//           targetDate: new Date('2022-10-24'),
+//           targetAmount: 200,
+//           currentAmount: 300,//This should only add 200, since that is the max amount (as dictated by target)
+//           userId: 'cberg@skillstorm.com',
+//           user: testUser
+//         }
+//       ]
+    
+//     });
+//   } else {
+//     let response = new HttpErrorResponse({status: 400});
+//   }
+//   return new Observable<HttpResponse<any>>({this: response})
+// }
