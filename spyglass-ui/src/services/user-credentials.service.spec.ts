78fecf847aafb9f0d8935148da3c247d801d023b
cjberg28 <cjberg28@yahoo.com>
2022-09-06 13:51:24 -0500

    User Credentials testing done

--- spyglass-ui/src/services/user-credentials.service.spec.ts
+++ spyglass-ui/src/services/user-credentials.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { User } from 'src/models/user.model';
+import { environment } from 'src/environments/environment';
 
 import { UserCredentialsService } from './user-credentials.service';
 
@@ -12,5 +14,22 @@ describe('UserCredentialsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it ('should set the user when setUser() is called', () => {
+    let newUser: User = new User('jtester@aol.com','Jonathan','Tester',new Date('2000/01/01'));
+    service.setUser(newUser);
+    expect(service.getUser()).toBe(newUser);
+  });
+
+  it ('should set the username when setUsername() is called', () => {
+    let newUsername: string = 'jtester@skillstorm.com';
+    service.setUsername(newUsername);
+    expect(service.getUsername()).toBe(newUsername);
+  });
+
+  it ('should set the password when setPassword() is called', () => {
+    service.setPassword(environment.TEST_PASSWORD);
+    expect(service.getPassword()).toBe(environment.TEST_PASSWORD);
   });
 });
