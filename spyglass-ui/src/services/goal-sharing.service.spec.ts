f46a9f7ac612a0317265e27a3c7bab8401c33d0b
cjberg28 <cjberg28@yahoo.com>
2022-09-06 12:26:31 -0500

    Goal Sharing Service testing done

--- spyglass-ui/src/services/goal-sharing.service.spec.ts
+++ spyglass-ui/src/services/goal-sharing.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { Goal } from 'src/models/goal.model';
+import { User } from 'src/models/user.model';
 
 import { GoalSharingService } from './goal-sharing.service';
 
@@ -12,5 +14,11 @@ describe('GoalSharingService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it ('should set the goal when setGoal() is called', () => {
+    let newGoal: Goal = new Goal(1,'Name','Description','imageSrc',new Date('2000/01/28'),12,1,'',new User('','','',new Date('2001/01/28')));
+    service.setGoal(newGoal);
+    expect(service.getGoal()).toBe(newGoal);
   });
 });
