4058eee003af5ce350d8e282017bb371241abb4a
cjberg28 <cjberg28@yahoo.com>
2022-09-06 15:34:58 -0500

    Goal Service testing done????

--- spyglass-ui/src/services/goal.service.spec.ts
+++ spyglass-ui/src/services/goal.service.spec.ts
@@ -1,5 +1,8 @@
-import { HttpClientModule } from '@angular/common/http';
-import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { environment } from 'src/environments/environment';
+import { Goal } from 'src/models/goal.model';
+import { User } from 'src/models/user.model';
 
 import { GoalService } from './goal.service';
 
@@ -16,4 +19,48 @@ describe('GoalService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  //Is this the correct way to test these?
+  //This more or less checks if the functions are called, not if the data being returned matches expected output.
+  it ('should find all goals', () => {
+    let http = TestBed.inject(HttpClient);
+    spyOn(http, 'get').and.callThrough();
+    let result = service.findAllGoals(environment.TEST_USERNAME, environment.TEST_PASSWORD);
+    expect(http.get).toHaveBeenCalled();
+    expect(result).toBeTruthy();
+  });
+
+  it ('should find by ID', () => {
+    let http = TestBed.inject(HttpClient);
+    spyOn(http, 'get').and.callThrough();
+    let result = service.findById(1, environment.TEST_USERNAME, environment.TEST_PASSWORD);
+    expect(http.get).toHaveBeenCalled();
+    expect(result).toBeTruthy();
+  });
+
+  it ('should create a goal', () => {
+    let http = TestBed.inject(HttpClient);
+    spyOn(http, 'post').and.callThrough();
+    let goal: Goal = new Goal(0,'Test Name','Test Description','images.com/example',new Date('2033/12/21'),20,10,environment.TEST_USERNAME,new User(environment.TEST_USERNAME,'Cameron','Berg',new Date('2000/01/28')));
+    let result = service.createGoal(goal, environment.TEST_USERNAME, environment.TEST_PASSWORD);
+    expect(http.post).toHaveBeenCalled();
+    expect(result).toBeTruthy();
+  });
+
+  it ('should update a goal', () => {
+    let http = TestBed.inject(HttpClient);
+    spyOn(http, 'put').and.callThrough();
+    let goal: Goal = new Goal(1,'Test Name','Test Description','images.com/example',new Date('2033/12/21'),20,10,environment.TEST_USERNAME,new User(environment.TEST_USERNAME,'Cameron','Berg',new Date('2000/01/28')));
+    let result = service.updateGoal(goal,environment.TEST_USERNAME,environment.TEST_PASSWORD);
+    expect(http.put).toHaveBeenCalled();
+    expect(result).toBeTruthy();
+  });
+
+  it ('should delete a goal', () => {
+    let http = TestBed.inject(HttpClient);
+    spyOn(http, 'delete').and.callThrough();
+    let result = service.deleteGoal(1,environment.TEST_USERNAME,environment.TEST_PASSWORD);
+    expect(http.delete).toHaveBeenCalled();
+    expect(result).toBeTruthy();
+  })
 });
